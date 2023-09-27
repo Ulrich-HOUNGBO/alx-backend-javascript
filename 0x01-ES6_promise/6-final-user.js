3355f29a6a72fdfff13aa2abae1ff48761045e06
Ulrich HOUNGBO <houngbou@gmail.com>
2023-09-27 01:01:45 +0100

    fixe eslint

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,16 +2,22 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  const promises = [signUpUser(firstName, lastName), uploadPhoto(fileName)];
-  return Promise.allSettled(promises).then((values) => {
-    const results = [];
-    values.forEach((value) => {
-      if (value.status === 'fulfilled') {
-        results.push(value.value);
-      } else {
-        results.push(value.reason.toString());
-      }
+  const promises = [signUpUser(firstName, lastName),
+    uploadPhoto(fileName)];
+
+  return Promise.allSettled(promises)
+    .then((values) => {
+      const res = [];
+      values.forEach((value) => {
+        if (value.status === 'fulfilled') {
+          res.push(value);
+        } else {
+          res.push({
+            status: value.status,
+            value: `Error: ${value.reason.message}`,
+          });
+        }
+      });
+      return res;
     });
-    return results;
-  });
 }
