64847a60cb4b943a68325bf6c570ed9098ea0425
Ulrich HOUNGBO <houngbou@gmail.com>
2023-11-21 17:35:33 +0100

    node js

---
+++ 0x00-ES6_basic/3-main.js
@@ -0,0 +1,7 @@
+import getSumOfHoods from "./3-default-parameter";
+
+
+console.log(getSumOfHoods(34));
+console.log(getSumOfHoods(34, 3));
+console.log(getSumOfHoods(34, 3, 4));
+
---
+++ 0x00-ES6_basic/4-main.js
@@ -0,0 +1,5 @@
+import returnHowManyArguments from './4-rest-parameter';
+
+console.log(returnHowManyArguments('one', 'two'));
+console.log(returnHowManyArguments('one', 'two', 'three'));
+console.log(returnHowManyArguments('one', 'two', 'three', 'four'));
