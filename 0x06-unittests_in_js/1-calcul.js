beda03cc0365820e40c0c72c9c112b7c0f8ceac1
Ulrich HOUNGBO <houngbou@gmail.com>
2023-11-25 18:25:07 +0100

    add type operation in test case

--- 0x06-unittests_in_js/1-calcul.js
+++ 0x06-unittests_in_js/1-calcul.js
@@ -1,12 +1,18 @@
-const calculateNumber=(type, a, b) => {
-	if (type === 'SUM'){
-		return Math.round(a) + Math.round(b)
-	}
-	else if (type === 'SUBTRACT') {
-		return Math.round(a) - Math.round(b)
-	}
-	else if (type === 'DIVIDE') {
-		return Math.round(a) / Math.round(b)
-	}
+'use strict';
+const calculateNumber = (type, a, b) => {
+	switch (type) {
+		case 'SUM':
+			return Math.round(a) + Math.round(b);
+			break;
+		case 'SUBTRACT':
+			return Math.round(a) - Math.round(b);
+			break;
+		case 'DIVIDE':
+			if (Math.round(b) === 0) return 'Error';
+			return Math.round(a) / Math.round(b);
+			break;
+		default:
+			break;
+	};
 }
 module.exports = calculateNumber;
