dbb3d374988e25233adb76b7c1da94e6312fa432
Ulrich HOUNGBO <houngbou@gmail.com>
2023-11-26 02:23:32 +0100

    create stub for test

--- 0x06-unittests_in_js/4-payment.test.js
+++ 0x06-unittests_in_js/4-payment.test.js
@@ -3,18 +3,18 @@ const chai = require('chai');
 const sinon = require('sinon');
 
 const Utils = require('./utils.js');
-const sendPaymentRequestToApi = require('./3-payment.js');
+const sendPaymentRequestToApi = require('./4-payment.js');
 
 describe('sendPaymentRequestToApi function', () => {
-    const spyUtils = sinon.spy(Utils, 'calculateNumber');
+    const spyConsole = sinon.spy(console, 'log');
 
     it('validate the usage of the Utils function', () => {
         const stubUtils = sinon.stub(Utils, 'calculateNumber');
         stubUtils.withArgs('SUM', 100, 20).returns(10);
         sendPaymentRequestToApi(100, 20);
-        chai.expect(spyUtils.calledOnce).to.be.true;
-        chai.expect(spyUtils.calledWith('SUM', 100, 20)).to.be.true;
-        stubUtils.restore();
-        spyUtils.restore()
+        chai.expect(spyConsole.calledOnce).to.be.true;
+        chai.expect(spyConsole.calledWith('The total is: 10')).to.be.true;
+        stubUtils.restore()
+        spyConsole.restore();
     });
 });
